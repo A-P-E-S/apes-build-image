0dad339f032d5354a2afe4ad043324b9c6fe7dd0
Luke Joshua Park <_+github@lukejoshuapark.io>
2023-12-22 12:51:24 +1300

    Fixed issue with migrate install

--- scripts/install-migrate.sh
+++ scripts/install-migrate.sh
@@ -2,7 +2,7 @@
 
 VERSION=4.16.2
 
-wget https://github.com/golang-migrate/migrate/releases/download/v4.16.2/migrate.linux-amd64.tar.gz || exit 1
+wget https://github.com/golang-migrate/migrate/releases/download/v4.16.2/migrate.linux-${TARGETARCH}.tar.gz || exit 1
 tar -xf migrate.linux-${TARGETARCH}.tar.gz || exit 1
 chmod u+x migrate || exit 1
 mv migrate /usr/bin/migrate || exit 1
