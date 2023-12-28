40e02916c0b3a7fdbd9efc118a353d6554f91765
Luke Joshua Park <_+github@lukejoshuapark.io>
2023-12-29 08:55:41 +1300

    Upgraded to Rust 1.75

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=1.74.1
+VERSION=1.75.0
 
 (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain $VERSION) || exit 1
 $HOME/.cargo/bin/rustup component add clippy || exit 1
